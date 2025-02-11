b6980ed23f72cb9aade8effe3152a8d1c6f5da2b
xgfone <xgfone@126.com>
2025-02-11 23:25:34 +0800

    feishu webhook driver supports the pre-defined secrets

--- driver/drivers/feishu/webhook.go
+++ driver/drivers/feishu/webhook.go
@@ -17,6 +17,7 @@ package feishu
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/xgfone/go-msgnotice/driver"
 	"github.com/xgfone/go-msgnotice/driver/builder"
@@ -31,9 +32,21 @@ func init() { builder.NewAndRegister(DriverTypeWebhook, NewWebhook) }
 // NewWebhook returns a new driver, which sends the message to feishu.
 //
 // The optional secret is extracted from the message metadata by name "secret".
-func NewWebhook(_ map[string]any) (driver.Driver, error) {
+func NewWebhook(config map[string]any) (driver.Driver, error) {
+	_secrets := config["secrets"].(map[string]any)
+	secrets := make(map[string]string, len(_secrets))
+	for k, v := range _secrets {
+		var ok bool
+		if secrets[k], ok = v.(string); !ok {
+			return nil, fmt.Errorf("secret of '%s' is %T, not string", k, v)
+		}
+	}
+
 	return driver.New(DriverTypeWebhook, func(c context.Context, m driver.Message) error {
-		secret, _ := m.Metadata["secret"].(string)
+		secret, ok := m.Metadata["secret"].(string)
+		if !ok {
+			secret = secrets[m.Receiver]
+		}
 		return feishu.NewWebhook(m.Receiver, secret).SendText(c, m.Content)
 	}, nil), nil
 }
